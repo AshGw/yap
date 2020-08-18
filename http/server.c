4b26f46655d489496ff7c49072e69834e3541d26
AshGw <AshrefGw@proton.me>
2020-08-18 12:15:10 +0100

    server works good

--- http/server.c
+++ http/server.c
@@ -11,26 +11,42 @@
 typedef struct sockaddr_in sockaddr_in_t;
 
 int main() {
-    int file_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if (file_desc == -1) {
+    int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1) {
         perror("Socket creation failed");
-        exit(1);
+        exit(-1);
     }
     sockaddr_in_t server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
-    int bind_err = bind(file_desc, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    int bind_err = bind(socket_desc, (struct sockaddr *)&server_addr, sizeof(server_addr));
     if (bind_err){
-        perror("Bind failed");
-        exit(1);
+        perror("binding failed");
+        exit(-1);
     }
-    int listen_err = listen(file_desc, MAX_CONNECTIONS);
+    int listen_err = listen(socket_desc, MAX_CONNECTIONS);
     if (listen_err) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
+        perror("listening failed");
+        exit(-1);
     }
 
-    close(file_desc);
+    printf("Server listening on http://localhost:%d\n", PORT);
+    sockaddr_in_t client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+
+     while (1) {
+        int new_socket_desc = accept(socket_desc, (struct sockaddr *)&client_addr, &addr_len);
+        if (new_socket_desc) {
+            perror("accept failed");
+            continue;
+        }
+        printf("connection accepted from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        send(new_socket_desc, RESPONSE, strlen(RESPONSE), 0);
+        close(new_socket_desc);
+    }
+
+    close(socket_desc);
     return 0;
 }
