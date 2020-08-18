8090380a44c5073585bc8d8291fb7af28ca3c04a
AshGw <AshrefGw@proton.me>
2020-08-18 10:14:40 +0100

    setup

---
+++ http/server.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8888
+#define MAX_CONNECTIONS 10
+#define RESPONSE "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, World!</h1></body></html>"
+
+int main() {
+    // running
+    return 0;
+}
