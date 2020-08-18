a4bce218819909f8835e78e80e413d8adb49f211
AshGw <AshrefGw@proton.me>
2020-08-18 10:15:40 +0100

    sockets created

--- http/server.c
+++ http/server.c
@@ -6,9 +6,14 @@
 
 #define PORT 8888
 #define MAX_CONNECTIONS 10
-#define RESPONSE "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, World!</h1></body></html>"
+#define RESPONSE "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Ok this is super fast frfr!</h1></body></html>"
 
 int main() {
-    // running
+    int file_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (file_desc == -1) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+    close(file_desc);
     return 0;
 }
